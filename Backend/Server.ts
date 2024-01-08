d222bd42ba3d800ad528c59da8c29a9d9da80a99
KyEBell <kyleevanbell@gmail.com>
2024-01-08 17:13:42 -0500

    npm installed cors and used on server.ts, frontend login route now works through the backed

--- Backend/Server.ts
+++ Backend/Server.ts
@@ -6,6 +6,7 @@ import loginRoute from './routes/loginRoute';
 import signUpRoute from './routes/signUpRoute';
 import dotenv from 'dotenv';
 import path from 'path';
+import cors from 'cors';
 
 dotenv.config({ path: path.resolve(__dirname, './../.env') });
 
@@ -13,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 //initial app.use calls------------------------------------------------->
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

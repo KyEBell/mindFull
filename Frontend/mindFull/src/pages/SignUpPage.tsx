c15cf3567df103c2322db73630425c2fafabb39f
KyEBell <kyleevanbell@gmail.com>
2024-01-12 17:56:51 -0500

    signupFormBranch being pushed to prevent any loss of code to the dev branch

--- Frontend/mindFull/src/pages/SignUpPage.tsx
+++ Frontend/mindFull/src/pages/SignUpPage.tsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+interface SignUpPageProps {
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface SignUpForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 const SignUpPage: React.FC = () => {
-  return <h1>SIGNUP PAGE</h1>;
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState<SignUpForm>({
+    username: '',
+    password: '',
+    email: '',
+  });
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log('formData', formData);
+  };
+
+  return (
+    <>
+      <h2>Create an account</h2>
+      <form>
+        <label>
+          Email:
+          <input
+            type='email'
+            name='email'
+            value={formData.email}
+            onChange={handleFormChange}
+          />
+        </label>
+        <label>
+          Username:
+          <input
+            type='text'
+            name='username'
+            value={formData.username}
+            onChange={handleFormChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type='password'
+            name='password'
+            value={formData.password}
+            onChange={handleFormChange}
+          />
+        </label>
+      </form>
+    </>
+  );
 };
 
 export default SignUpPage;
